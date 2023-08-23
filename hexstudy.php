18ab587bd99489c15e1d34dd22533e27e6eb1f52
Evgeniy Korablev <VeseliyJmih@yandex.ru>
2023-08-23 14:45:50 +0300

    21 урок и новая ветка

--- hexstudy.php
+++ hexstudy.php
@@ -91,3 +91,22 @@ function getAgeDifference($year1, $year2){
 
 $actual = getAgeDifference($year1, $year2);
 print_r($actual);
+/////////////////////////////////////////////////////////////
+/// 21e задание
+/// Реализуйте функцию getFormattedBirthday(), которая принимает на вход три параметра:
+/// день, месяц и год рождения. Она возвращает их строкой в отформатированном виде, например: 20-02-1953.
+/// $result = getFormattedBirthday(20, 2, 1953);
+/// print_r($result); // => 20-02-1953
+/// День и месяц нужно форматировать так, чтобы при необходимости добавлялся 0 слева.
+/// Например, если в качестве месяца пришла цифра 7, то в выходной строке она должна быть представлена как 07.
+$day = rand(1, 31);
+$month = rand(1, 12);
+$year = rand(1950, 2023);
+
+function getFormattedBirthday($day, $month, $year){
+    $date = sprintf( '%02d-%02d-%04d', $day, $month, $year);
+    return $date;
+}
+
+$result = getFormattedBirthday($day, $month, $year);
+print_r($result);
