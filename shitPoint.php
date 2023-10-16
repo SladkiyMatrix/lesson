ecfb02276767044f0485e53f30957ec1e3783043
SladkiyMatrix <VeseliyJmih@yandex.ru>
2023-10-16 17:42:26 +0300

    не работает continue

--- shitPoint.php
+++ shitPoint.php
@@ -1,139 +1,13 @@
 <?php
-/*$russia = [
-    'Московская область' => ['Москва', 'Зеленоград', 'Клин', 'Коломна'],
-    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
-    'Рязанская область' => ['Рязань', 'Михайлов', 'Кораблино', 'Ряжск'],
-    'Тверская область' => ['Тверь', 'Ржев', 'Торжок', 'Бологое']
-];
 
-foreach($russia as  $towns) {
-
-    foreach ($towns as  $town) {
-
-        if(strstr($town, "К")) {
-
-            $s = ' ';
-            $kay = $town . $s;
-            //echo $kay;
-            $kayArr = explode(' ', $kay);
-            foreach ($kayArr as $k){
-                if($k == end($kayArr)){
-                    echo $k . '.';
-                }
-                echo $k . ',';
-            }
-
-            }
-
-        }
-    }*/
-/*$russia = [
-    'Московская область' => ['Москва', 'Зеленоград', 'Клин', 'Коломна', 'Карлик'],
-    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
-    'Рязанская область' => ['Рязань', 'Михайлов', 'Кораблино', 'Ряжск'],
-    'Тверская область' => ['Тверь', 'Ржев', 'Торжок', 'Бологое'],
-];
-
-$townlist = '';
-
-foreach ($russia as $towns) {
-
-    foreach ($towns as $town) {
-
-        if (stristr($town, "к")) {
-            $townlist = $townlist . $town . ',';
-        }
-    }
-}
-$townsk = explode(',', $townlist);
-print_r($townsk);
-echo "<br>";
-
-$townlistK = '';
-foreach ($russia as $towns) {
-
-    foreach ($towns as $town) {
-
-        if (strstr($town, "К")) {
-            $townlistK = $townlistK . $town . ',';
-        }
-    }
-}
-
-$townsK= explode(',', $townlistK);
-print_r($townsK);
-$array = array_unique (array_merge ($townsk, $townsK));//объединение массивов с заменой повторений
-echo "<br>";
-print_r($array);
-echo "<br>";
-unset($array[7]);
-echo "<br>";
-$str = implode(', ', $array);
-echo $str . '.';
-echo "<br>";*/
-/*$russia = [
-    'Московская область' => ['Москва', 'кабанино', 'Зеленоград', 'Клин', 'Коломна', 'Карлик', 'кременчуг'],
-    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт', 'кировск', ],
-    'Рязанская область' => ['Рязань', 'Михайлов', 'Кораблино', 'Ряжск', 'кожемяка', 'кожный червь'],
-    'Тверская область' => ['Тверь', 'Ржев', 'Торжок', 'Бологое'],
-];
-$townliSthit = '';
-foreach ($russia as $towns) {
-
-    foreach ($towns as $town) {
-        array_map('strtolower', $towns);
-
-        if (strstr($town, "к")) {
-
-            $townliSthit = $townliSthit . $town . ',';
-        }
-    }
-}
-echo $townliSthit;*/
-//Добавление калоприёмки, честно спизженное регулярное выражение
-/*$russia = [
-    'Московская область' => ['Москва', 'кабанино', 'Зеленоград', 'Клин', 'Коломна', 'Карлик', 'кременчуг'],
-    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт', 'кировск', ],
-    'Рязанская область' => ['Рязань', 'Михайлов', 'Кораблино', 'Ряжск', 'кожемяка', 'кожный червь'],
-    'Тверская область' => ['Тверь', 'Ржев', 'Торжок', 'Бологое', 'Новая Калоприемка'],
-];
-$townListShit = '';
-foreach ($russia as $towns) {
-
-    foreach ($towns as $town) {
-
-        if (preg_match_all('/(^|\s|\b)+[К]+/u', $town)) {
-
-            $townListShit = $townListShit . $town . ',';
-        }
-    }
-}
-echo $townListShit;
-echo "<br>";
-$townListShit1 = '';
-foreach ($russia as $towns) {
-
-    foreach ($towns as $town) {
-
-        if (preg_match_all('/(^|\s|\b)+[к]+/u', $town)) {
-
-            $townListShit1 = $townListShit1 . $town . ',';
-        }
-    }
-}
-echo $townListShit1;
-echo "<br>";
-$s = $townListShit1 . $townListShit;
-$shit = substr($s, 0, -1);
-echo $shit . '.';*/
 //ща нормально сделаем
 $russia = [
     'Московская область' => ['Москва', 'кабанино', 'Зеленоград', 'Клин', 'Коломна', 'Карлик', 'кременчуг'],
     'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт', 'кировск', ],
-    'Рязанская область' => ['Рязань', 'Михайлов', 'Кораблино', 'Ряжск', 'кожемяка', 'кожный червь'],
-    'Тверская область' => ['Тверь', 'Ржев', 'Торжок', 'Бологое', 'Новая Калоприемка', 'нижние залупки'],
+    'Рязанская область' => ['Рязань', 'Михайлов', 'Кораблино', 'Ряжск', 'кожемяка', 'кожный червь', 'Кашино-Залупино'],
+    'Тверская область' => ['Тверь', 'Ржев', 'Торжок', 'Бологое', 'Новая Калоприемка', 'нижние залупки', 'Верхний-Кал'],
 ];
-$townListShit = '';
+/*$townListShit = '';
 foreach ($russia as $towns) {
 
     foreach ($towns as $town) {
@@ -153,4 +27,56 @@ foreach ($russia as $towns) {
         }
     }
 }
-echo $townListShit;
+echo $townListShit;*/
+echo townsearch($russia, 'к');
+
+
+function townsearch($arraytown, $needle): string {
+    $separators = [' ', '-'];
+
+    $upperNeedle = mb_strtoupper($needle);
+    $lowerNeedle = mb_strtolower($needle);
+    $townListShit = '';
+
+    //Перебираем основной массив
+    foreach ($arraytown as $towns) {
+
+        //Перебираем массив в массиве
+        foreach ($towns as $town){
+
+            //перебираем массив с разделителями $seporators
+            foreach($separators as $seporator){
+
+                // проверям есть ли разделитель в словах
+                if(str_contains($town, $seporator)){
+
+                    //если нашли то разбираем слова по разделителю
+                    $townSeporator = explode($seporator, $town);
+
+                    //перебираем слова из значений массива $townSeporator
+                    foreach ($townSeporator as $townS){
+
+                        //Здесь проверяем есть ли заданный символ из $needle в начале каждого слова
+                        if ((str_starts_with($townS, $upperNeedle)) || (str_starts_with($townS, $lowerNeedle))) {
+                            //Добавляем в список
+                            $townListShit = $townListShit . $town . ',';
+                            //var_dump($townListShit);
+
+                        }
+
+                    }
+
+                }
+                continue;
+            }
+
+            //Здесь проверяем на наличие нужного символа в начале каждого слова
+            if ((str_starts_with($town, $upperNeedle)) || (str_starts_with($town, $lowerNeedle))) {
+                //Добавляем в список
+                $townListShit = $townListShit . $town . ',';
+            }
+        }
+
+    }
+    return $townListShit;
+}
