8732b3b33a2eb92647e30936369cd43a3572fbdc
SladkiyMatrix <VeseliyJmih@yandex.ru>
2023-10-17 05:59:01 +0300

    добавил функцию, починил continue

--- shitPoint.php
+++ shitPoint.php
@@ -1,36 +1,30 @@
 <?php
 
-//ща нормально сделаем
 $russia = [
     'Московская область' => ['Москва', 'кабанино', 'Зеленоград', 'Клин', 'Коломна', 'Карлик', 'кременчуг'],
     'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт', 'кировск', ],
     'Рязанская область' => ['Рязань', 'Михайлов', 'Кораблино', 'Ряжск', 'кожемяка', 'кожный червь', 'Кашино-Залупино'],
     'Тверская область' => ['Тверь', 'Ржев', 'Торжок', 'Бологое', 'Новая Калоприемка', 'нижние залупки', 'Верхний-Кал'],
 ];
-/*$townListShit = '';
-foreach ($russia as $towns) {
 
-    foreach ($towns as $town) {
-        $space = ' ';
-        if(str_contains($town, $space)){
-            $townSpace = explode(' ', $town);
-            foreach ($townSpace as $townS) {
-                if (str_starts_with($townS, 'к') || (str_starts_with($townS, 'К'))) {
-                    $str = implode(' ', $townSpace);
-                    $townListShit = $townListShit . $str . ',';
-                }
-            }
-        }
-        if (str_starts_with($town, 'к') || (str_starts_with($town, 'К'))) {
-
-                $townListShit = $townListShit . $town . ',';
-        }
-    }
+/**
+ * @param $town 
+ * @param $upperNeedle
+ * @param $lowerNeedle
+ * @return bool
+ */
+function lettersearch($town, $upperNeedle, $lowerNeedle){
+   return (str_starts_with($town, $upperNeedle)) || (str_starts_with($town, $lowerNeedle));
 }
-echo $townListShit;*/
+
 echo townsearch($russia, 'к');
 
 
+/**
+ * @param $arraytown //массив городов
+ * @param $needle // буква на которую будем искать
+ * @return string
+ */
 function townsearch($arraytown, $needle): string {
     $separators = [' ', '-'];
 
@@ -57,21 +51,18 @@ function townsearch($arraytown, $needle): string {
                     foreach ($townSeporator as $townS){
 
                         //Здесь проверяем есть ли заданный символ из $needle в начале каждого слова
-                        if ((str_starts_with($townS, $upperNeedle)) || (str_starts_with($townS, $lowerNeedle))) {
+                        if (lettersearch($townS, $upperNeedle, $lowerNeedle)) {
                             //Добавляем в список
                             $townListShit = $townListShit . $town . ',';
-                            //var_dump($townListShit);
-
+                            //цифра после continue - на скольких уровнях вложенных циклов будет пропущена оставшаяся часть итерации
+                            continue 3;
                         }
-
                     }
-
                 }
-                continue;
             }
 
             //Здесь проверяем на наличие нужного символа в начале каждого слова
-            if ((str_starts_with($town, $upperNeedle)) || (str_starts_with($town, $lowerNeedle))) {
+            if (lettersearch($town, $upperNeedle, $lowerNeedle)) {
                 //Добавляем в список
                 $townListShit = $townListShit . $town . ',';
             }
