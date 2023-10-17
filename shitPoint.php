1090ced39695a5784f38552367929f129e3a5098
SladkiyMatrix <VeseliyJmih@yandex.ru>
2023-10-17 20:10:01 +0300

    сделал через глобальную переменную

--- shitPoint.php
+++ shitPoint.php
@@ -1,5 +1,5 @@
 <?php
-
+// массив городов
 $russia = [
     'Московская область' => ['Москва', 'кабанино', 'Зеленоград', 'Клин', 'Коломна', 'Карлик', 'кременчуг'],
     'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт', 'кировск', ],
@@ -8,23 +8,23 @@ $russia = [
 ];
 
 /**
- * @param $town 
- * @param $upperNeedle
- * @param $lowerNeedle
+ * @param $town // переменная которую перебираем в массиве
+ * @param $upperNeedle // буква в верхнем регистре
+ * @param $lowerNeedle // буква в нижнем регистре
  * @return bool
  */
+//функция для проверки с какой буквы начинается перемнная
 function lettersearch($town, $upperNeedle, $lowerNeedle){
    return (str_starts_with($town, $upperNeedle)) || (str_starts_with($town, $lowerNeedle));
 }
-
-echo townsearch($russia, 'к');
-
+//echo townsearch($russia, 'к');
 
 /**
  * @param $arraytown //массив городов
  * @param $needle // буква на которую будем искать
  * @return string
  */
+//функция поиска городов из массива $arraytown, на введенную пользователем букву $needle
 function townsearch($arraytown, $needle): string {
     $separators = [' ', '-'];
 
@@ -50,7 +50,7 @@ function townsearch($arraytown, $needle): string {
                     //перебираем слова из значений массива $townSeporator
                     foreach ($townSeporator as $townS){
 
-                        //Здесь проверяем есть ли заданный символ из $needle в начале каждого слова
+                        //Здесь проверяем есть ли заданный символ из $needle в начале каждого слова, функцией lettersearch
                         if (lettersearch($townS, $upperNeedle, $lowerNeedle)) {
                             //Добавляем в список
                             $townListShit = $townListShit . $town . ',';
@@ -61,7 +61,7 @@ function townsearch($arraytown, $needle): string {
                 }
             }
 
-            //Здесь проверяем на наличие нужного символа в начале каждого слова
+            //Здесь проверяем на наличие нужного символа в начале каждого слова, функцией lettersearch
             if (lettersearch($town, $upperNeedle, $lowerNeedle)) {
                 //Добавляем в список
                 $townListShit = $townListShit . $town . ',';
@@ -71,3 +71,40 @@ function townsearch($arraytown, $needle): string {
     }
     return $townListShit;
 }
+// maxlength=1 можно ограничить в форме ввод символов, но это слишком просто
+$form = '
+        <form method="get">
+            <p>Введите первую букву города, который хотите найти</p>
+            <p><input type="text" name="town"  placeholder="Введите первую букву" value="' . $_GET["town"] . '"/></p>
+            <input type="submit" value="Найти">
+            
+        </form>';
+//вывод формы на экран
+echo $form;
+//переменная принимает значение которое ввёл пользователь
+$letterTown = $_GET["town"];
+//засунул функцию в  перемнную, что бы вызвать в функции проверки на входищие символы от пользователя(lenghtown($letterTown))
+$funcSearch = townsearch($russia, $letterTown);
+
+/**
+ * @param $letterTown // буква введенная пользователем
+ * @return string
+ */
+// функция проверки введенной пользователем буквы
+function lenghtown($letterTown): string {
+    //проверка iconv_strlen проверяет введен ли 1 символ, а регулярное выражение - введен ли на кириллице
+    // отличие iconv от strlen в том что strlen проверяет на количество байтов, а в iconv можно указать кодировку и она проверяет именно символ
+    if( (iconv_strlen($letterTown, 'UTF-8')) == 1 && (preg_match("/^[А-Яа-я]+$/u", $letterTown))){
+        // сделал переменную глобальной что бы вызвать её без ошибок в функции, другого способа не нашел
+        //точнее нашел, но там класс использовать предлагали, потм побольше про них прочитаю
+        global $funcSearch;
+        //возвращает нашу функцию писка городов на нужную нам букву
+        return $funcSearch;
+
+    }else{
+        $errorForm = 'это не буква русского алфавита';
+        //возвраает строку, если введенный символ не прошел проверку
+        return $errorForm;
+    }
+}
+echo lenghtown($letterTown);
