256dc4295f4ec989b3c251e9ad1ad3b66e0f9829
SladkiyMatrix <VeseliyJmih@yandex.ru>
2023-11-22 20:51:22 +0300

    +

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DBUSERNAME', 'root'); // имя пользователя
 define('DBPASSWORD', ''); // пароль
 define('DBNAME', 'hehe'); // название базы
 
-/* соединяемся с базой */
+//соединение с базой
 $db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
 
 // проверяем соединение
@@ -12,32 +12,42 @@ $db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
 if($db === false){
     die("Ошибка соединения с базой. " . mysqli_connect_error());
 }*/
-//если перемення попала в connect_error
+//проверка соединения, если есть ошибки при установке соединения, то завершаем скрипт и выводим сообщение с ошибкой
 if ($db->connect_error) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Ошибка подключения: " . mysqli_connect_error());
 }
-//создаем таблицу с именем new_user и полями
+//создаем таблицу с именем users и полями
 $sql = "
-CREATE TABLE new_users (
-    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `name` varchar(75) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `email` varchar(100) NOT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE users (
+    users_id int(11)  NOT NULL AUTO_INCREMENT,
+    name varchar(75) NOT NULL,
+    password varchar(255) NOT NULL,
+    email varchar(100) NOT NULL,
+    PRIMARY KEY (users_id)
     );";
 //делаем запрос
 if (mysqli_query($db,$sql)) {
     //если все нормально
-    echo "База данных успешно создана";
+    echo "таблица успешно создана ";
 } else {
     //если ничего не нормально  выводим ошибку
-    echo "Ошибка " . mysqli_error($db);
+    echo "Ошибка: " . mysqli_error($db) . ' |';
+}
+
+// проверяем есть ли интересующая наст таблица в БД
+//название таблицы
+$tableName = 'users';
+//запрос к структуре таблицы с именем таблицы $tableName
+$query = "DESCRIBE $tableName";
+//сохраняем запрос в переменную
+$resultQuery = mysqli_query($db, $query);
+//проверяем есть ли таблица
+if ($resultQuery->num_rows > 0){
+    echo ' таблица ' . $tableName . ' сущствует';
 }
 //закрыли соединение
 //mysqli_close($db);
 
-//Сообщение об ошибка, которые у нас могут возникнуть, вначалае пустое
-$error = ' ';
 //наша форма регистрации
 $form = '
         <form action="" method="POST">
@@ -68,25 +78,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //проверяе заполнены ли все поля перед отправкой формы
     if (empty($name . $email . $password . $confirm_password)) {
         $fullError = 'Вы не заполнили поля' . "<br>";
-    }else {
+        echo $fullError;
+    } else {
         //теперь поверяем все поля по отдельности на то какие из них не заполнены
         if (empty($name)) {
             $nameError = 'Введите  имя' . "<br>";
+            echo  $nameError;
         }
         if (empty($email)) {
             $emailError = 'Введите email' . "<br>";
+            echo $emailError;
         }
         if (empty($password)) {
             $passwordError = 'Введите  пароль' . "<br>";
-        }
+            echo $passwordError;
+        } else {
+            if (strlen($password) < 3) {
+                $error = 'пароль не может быть меньше 3х символов'. "<br>";
+                echo $error;
+            }
+
+                }
         if (empty($confirm_password)) {
-            $confirm_passwordError = 'Введите  пароль второй раз' . "<br>";
+            $confirm_passwordError = 'подтвердите пароль' . "<br>";
+            echo $confirm_passwordError;
+        } else {
+            if ($password != $confirm_password ){
+                echo 'пароли не совпадают';
+            }
         }
+
     }
-    //выводим ошибки если есть
-    echo $fullError;
-    echo  $nameError;
-    echo $emailError;
-    echo $passwordError;
-    echo $confirm_passwordError;
+
 }
